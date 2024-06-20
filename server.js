6a61cd5a7ef330b5dcef21a69517765a63d80abd
jayak <kumarijaya127@gmail.com>
2024-06-20 11:02:11 +0530

    post route added

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const app = express();
+const userModel = require("./models/user");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("welcome to category page");
 });
 
-app.get("/home", (req, res) => {
-  res.send("welcome to Home page");
+app.get("/category", async (req, res) => {
+  const { categoryName, categoryDescription } = req.body;
+  const category = await userModel.find({
+    categoryName,
+    categoryDescription,
+  });
+  res.send(category);
+});
+
+app.post("/category", async (req, res) => {
+  const { categoryName, categoryDescription } = req.body;
+  const newCategory = new userModel({
+    categoryName,
+    categoryDescription,
+  });
+  await newCategory.save(); // Save new category to database
+  res.status(201).send(newCategory);
 });
 
 const PORT = process.env.PORT || 3000;
