9aab465bafc7bcb7d2caa332118db5ec96cf02b9
jayak <kumarijaya127@gmail.com>
2024-06-21 15:48:16 +0530

    styles added

--- frontend/src/components/ViewCategory.jsx
+++ frontend/src/components/ViewCategory.jsx
@@ -1,6 +1,7 @@
 // import { Link, NavLink } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import "./ViewCategory.css";
 
 const ViewCategory = () => {
   const [category, setCategory] = useState([]);
@@ -14,23 +15,26 @@ const ViewCategory = () => {
 
   return (
     <>
-      <table>
-        <tr>
-          <th>Category Name</th>
-          <th>Category Description</th>
-        </tr>
+      <h2 className="text-center mt-5 text-2xl text-sky-800">View Category</h2>
+      <div className="flex justify-center mt-5">
+        <table className="text-center my_table">
+          <tr>
+            <th>Category Name</th>
+            <th>Category Description</th>
+          </tr>
 
-        {category.map((cat) => {
-          return (
-            <>
-              <tr>
-                <td>{cat.categoryName}</td>
-                <td>{cat.categoryDescription}</td>
-              </tr>
-            </>
-          );
-        })}
-      </table>
+          {category.map((cat) => {
+            return (
+              <>
+                <tr>
+                  <td>{cat.categoryName}</td>
+                  <td>{cat.categoryDescription}</td>
+                </tr>
+              </>
+            );
+          })}
+        </table>
+      </div>
     </>
   );
 };
