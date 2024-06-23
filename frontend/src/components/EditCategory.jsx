eefa1aa755bea25d77e11accda71c7a311069310
jayak <kumarijaya127@gmail.com>
2024-06-23 21:07:07 +0530

    edit category

--- frontend/src/components/EditCategory.jsx
+++ frontend/src/components/EditCategory.jsx
@@ -1,31 +1,53 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import "./AddCategory.css";
 
 const EditCategory = () => {
   const [category, setCategory] = useState([]);
-  const { id } = useParams();
-  console.log("AAA", id);
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryDescription, setDescription] = useState("");
 
-  useEffect(() => {
-    fetchCategory();
-  }, [id]);
-
-  const { categoryName, categoryDescription } = category;
-
-  const fetchCategory = async () => {
+  const formHandler = async () => {
     try {
-      const res = await axios.get(`http://localhost:3000/api/category/` + id);
-      setCategory(res.data);
+      await axios
+        .post("http://localhost:3000/api/add-categories", {
+          categoryName,
+          categoryDescription,
+        })
+        .then((res) => setCategory(res.data))
+        .catch((error) => console.log("error in fetching data", error));
     } catch (error) {
-      console.log("error in displaying single category");
+      console.log("error");
     }
   };
 
   return (
     <>
-      <div className="text-center mt-10 text-blue-600">
-        {categoryName}---{categoryDescription}
+      <h2 className="text-center mt-5 text-2xl text-sky-800">Add Category</h2>
+      <div className="flex justify-center mt-5 ">
+        <div className="card-body bg-slate-200 p-5 ">
+          <form onSubmit={formHandler}>
+            <label> Category Name: </label>
+            <input
+              type="text"
+              name="categoryName"
+              value={categoryName}
+              onChange={(e) => setCategoryName(e.target.value)}
+            />
+            <label> Category Description: </label>
+            <input
+              type="text"
+              name="categoryDescription"
+              value={categoryDescription}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <input
+              type="submit"
+              value="add category"
+              className="card-footer bg-sky-800 text-white"
+            />
+          </form>
+        </div>
       </div>
     </>
   );
