26c94d50177e88ae345372104f7426489ec980c7
jayak <kumarijaya127@gmail.com>
2024-06-24 11:05:37 +0530

    update category added

--- frontend/src/components/ViewCategory.jsx
+++ frontend/src/components/ViewCategory.jsx
@@ -5,7 +5,6 @@ import { useParams } from "react-router-dom";
 const ViewCategory = () => {
   const [category, setCategory] = useState([]);
   const { id } = useParams();
-  console.log("AAA", id);
 
   useEffect(() => {
     fetchCategory();
